abf095c7b7a89ed198b643e57dd5d25225c2bb39
serraguti <pacoserranox@gmail.com>
2024-04-03 09:15:32 +0200

    Add project files.

---
+++ WCFLogicaPrimerServicio/IMetodosVariosContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLogicaPrimerServicio
+{
+    [ServiceContract]
+    public interface IMetodosVariosContract
+    {
+        [OperationContract]
+        List<int> GetTablaMultiplicar(int numero);
+        [OperationContract]
+        string GetSaludo(string nombre);
+
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+
+        string MetodoInvisible();
+    }
+}
